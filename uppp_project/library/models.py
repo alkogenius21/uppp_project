4ea7c569fe9385658ae57bf1d8d75f27c8116d90
Николай Обухов <obuhov.n@edu.narfu.ru>
2023-04-10 05:28:39 +0300

    Поменял комментарии на английский язык, добавил таблицу для новостей

--- uppp_project/library/models.py
+++ uppp_project/library/models.py
@@ -1,33 +1,47 @@
+"""
+File with description of all database tables for MTV template
+"""
+
 from django.db import models
-
-# Create your models here.
-
 
 class Book(models.Model):
     """
-    Таблица с описанием элементов книги
+
+    Тable with info about book
 
     """
-    Book_Title = models.CharField(max_length=200) #Название книги
-    Book_Author = models.CharField(max_length=200) #Автор книги
-    Book_Genre = models.CharField(max_length=200) #Жанр книги
-    Book_Description = models.TextField() #Описание книги
-    Book_YearOfPublishing = models.IntegerField(max_length=4) #Год публикации(в числовом формате)
-    Book_ISBN = models.IntegerField(max_length=20) #Код ISBN
-    Book_Condition = models.BooleanField() #Состояние книги
-    Book_Image = models.ImageField() #Фотография книги
+    Book_Title = models.CharField(max_length=200) #name of book
+    Book_Author = models.CharField(max_length=200) #book author
+    Book_Genre = models.CharField(max_length=200) #book genre
+    Book_Description = models.TextField() #book about
+    Book_YearOfPublishing = models.IntegerField(max_length=4) #Public Date(integer ver)
+    Book_ISBN = models.IntegerField(max_length=20) #ISBN code
+    Book_Condition = models.BooleanField() #condition of book
+    Book_Image = models.ImageField() #book photo
 
 class User(models.Model):
     """
 
-    Таблица с описанием элементов пользователя
+    Table with user information
 
     """
 
-    User_Surname = models.CharField(max_length=15) #Фамилия пользователя
-    User_Name = models.CharField(max_length=15) #Имя пользователя
-    User_Patronymic = models.CharField(max_length=15) #Отчество пользователя
-    User_DateOfBirth = models.DateField() #Дата рождения пользователя
-    User_PhoneNumber = models.CharField(max_length=12) #Номер телефона пользователя
-    User_Address = models.CharField(max_length=200) #Адрес пользователя
+    User_Surname = models.CharField(max_length=15) #user firstname
+    User_Name = models.CharField(max_length=15) #user second name
+    User_Patronymic = models.CharField(max_length=15) #user patrinymic
+    User_DateOfBirth = models.DateField() #date of birth
+    User_PhoneNumber = models.CharField(max_length=12) #phone number
+    User_Address = models.CharField(max_length=200) #user adress
+
+class News_paper(models.Model):
+    """
+
+    News information table
+
+    """
+    News_DateOfPub = models.DateTimeField() # date of publishing of article
+    News_Photo = models.ImageField() # photo for article
+    News_Article = models.TextField()
+    News_TitleOfArticle = models.CharField(max_length=40) # article title
+    News_ArticleAuthor = models.CharField(max_length=80) # article author
 
