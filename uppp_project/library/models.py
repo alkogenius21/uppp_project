e4f0e4647f7439b65ec50864e25ea2a94ee77fe0
Николай Обухов <obuhov.n@edu.narfu.ru>
2023-04-10 05:57:12 +0300

    Сделал первую миграцию бд(пришлось многое убрать и переделать). Джанго категорически отказывается принимать какие либо комментарии внутри кода models.py. Добавил в настройки проекта путь к приложению(пока что топорно, потом поработаю с корневыми путями). Сделал тестовую домашнюю страницу с выводом сообщения.

--- uppp_project/library/models.py
+++ uppp_project/library/models.py
@@ -5,43 +5,30 @@ File with description of all database tables for MTV template
 from django.db import models
 
 class Book(models.Model):
-    """
 
-    Òable with info about book
-
-    """
-    Book_Title = models.CharField(max_length=200) #name of book
-    Book_Author = models.CharField(max_length=200) #book author
-    Book_Genre = models.CharField(max_length=200) #book genre
-    Book_Description = models.TextField() #book about
-    Book_YearOfPublishing = models.IntegerField(max_length=4) #Public Date(integer ver)
-    Book_ISBN = models.IntegerField(max_length=20) #ISBN code
-    Book_Condition = models.BooleanField() #condition of book
-    Book_Image = models.ImageField() #book photo
+    Book_id = models.BigAutoField(primary_key=True)
+    Book_Title = models.CharField(max_length=200) 
+    Book_Author = models.CharField(max_length=200) 
+    Book_Genre = models.CharField(max_length=200) 
+    Book_Description = models.TextField() 
+    Book_YearOfPublishing = models.IntegerField() 
+    Book_ISBN = models.IntegerField() 
+    Book_Condition = models.BooleanField() 
 
 class User(models.Model):
-    """
 
-    Table with user information
-
-    """
-
-    User_Surname = models.CharField(max_length=15) #user firstname
-    User_Name = models.CharField(max_length=15) #user second name
-    User_Patronymic = models.CharField(max_length=15) #user patrinymic
-    User_DateOfBirth = models.DateField() #date of birth
-    User_PhoneNumber = models.CharField(max_length=12) #phone number
-    User_Address = models.CharField(max_length=200) #user adress
+    User_id = models.BigAutoField(primary_key=True)
+    User_Surname = models.CharField(max_length=15) 
+    User_Name = models.CharField(max_length=15) 
+    User_Patronymic = models.CharField(max_length=15) 
+    User_DateOfBirth = models.DateField() 
+    User_PhoneNumber = models.CharField(max_length=12) 
+    User_Address = models.CharField(max_length=200) 
 
 class News_paper(models.Model):
-    """
-
-    News information table
-
-    """
-    News_DateOfPub = models.DateTimeField() # date of publishing of article
-    News_Photo = models.ImageField() # photo for article
+    News_id = models.BigAutoField(primary_key=True)
+    News_DateOfPub = models.DateTimeField() 
     News_Article = models.TextField()
-    News_TitleOfArticle = models.CharField(max_length=40) # article title
-    News_ArticleAuthor = models.CharField(max_length=80) # article author
+    News_TitleOfArticle = models.CharField(max_length=40) 
+    News_ArticleAuthor = models.CharField(max_length=80) 
 
--- uppp_project/uppp_project/urls.py
+++ uppp_project/uppp_project/urls.py
@@ -3,8 +3,9 @@
 # Uncomment next two lines to enable admin:
 from django.contrib import admin
 from django.urls import path
-
+from library import views
 urlpatterns = [
     # Uncomment the next line to enable the admin:
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('', views.index, name='home')
 ]
