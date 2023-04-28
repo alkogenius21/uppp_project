215a36b26a2b998400be6b6bd3b950e826090be3
Николай Обухов <obuhov.n@edu.narfu.ru>
2023-04-28 16:11:02 +0300

    переделал базу данных, создал html файл страницы библиотекаря, сделал миграции БД

--- uppp_project/library/models.py
+++ uppp_project/library/models.py
@@ -6,29 +6,36 @@ from django.db import models
 
 class Book(models.Model):
 
-    Book_id = models.BigAutoField(primary_key=True)
     Book_Title = models.CharField(max_length=200) 
     Book_Author = models.CharField(max_length=200) 
     Book_Genre = models.CharField(max_length=200) 
     Book_Description = models.TextField() 
     Book_YearOfPublishing = models.IntegerField() 
-    Book_ISBN = models.IntegerField() 
-    Book_Condition = models.BooleanField() 
+    Book_ISBN = models.IntegerField()
+    Book_Photo = models.ImageField(upload_to='media/photos/&Genre/&Author', default='media/book_default.jpg')
+
+    def __str__(self):
+        return self.Book_Title
 
 class User(models.Model):
 
-    User_id = models.BigAutoField(primary_key=True)
     User_Surname = models.CharField(max_length=15) 
     User_Name = models.CharField(max_length=15) 
     User_Patronymic = models.CharField(max_length=15) 
-    User_DateOfBirth = models.DateField() 
-    User_PhoneNumber = models.CharField(max_length=12) 
-    User_Address = models.CharField(max_length=200) 
+    User_DateOfBirth = models.DateField()
+    User_PhoneNumber = models.CharField(max_length=12)
+    User_Mail = models.CharField()
+
+    def __str__(self):
+        return self.User_Mail
 
 class News_paper(models.Model):
-    News_id = models.BigAutoField(primary_key=True)
+
     News_DateOfPub = models.DateTimeField() 
     News_Article = models.TextField()
     News_TitleOfArticle = models.CharField(max_length=40) 
     News_ArticleAuthor = models.CharField(max_length=80) 
+
+    def __str__(self):
+        return self.News_Article
 
--- uppp_project/uppp_project/urls.py
+++ uppp_project/uppp_project/urls.py
@@ -1,11 +1,15 @@
-
+from uppp_project import settings
 
 # Uncomment next two lines to enable admin:
 from django.contrib import admin
 from django.urls import path
 from library import views
+from django.conf.urls.static import static
 urlpatterns = [
     # Uncomment the next line to enable the admin:
     path('admin/', admin.site.urls),
     path('', views.index)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
