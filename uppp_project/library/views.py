0aca517c221e9859bfb0d7f2c3f531218cf82389
Николай Обухов <obuhov.n@edu.narfu.ru>
2023-04-29 01:34:00 +0300

    сделал ссылки на страницу, добавил отдельное приложение для библиотекаря, чтобы не захламлять основной проект

--- uppp_project/library/views.py
+++ uppp_project/library/views.py
@@ -5,24 +5,28 @@ from django.http import HttpResponse
 
 from .models import *
  
-Nav_Tables = ["Главная", "Каталог", "Адрес", "История библиотеки"]
+Nav_Tables = [{'title': "Главная", 'url_name': 'home'},
+             {'title': "Каталог", 'url_name': 'catalog'},
+             {'title': "Адрес", 'url_name': 'adress'},
+             {'title': "История библиотеки", 'url_name': 'about'},
+             {'title': "Личный Кабинет", 'url_name': 'profile'}]
 
 def index(request):
     return render(request, 'index.html', {'menu': Nav_Tables, 'title': 'Вторая Кегостровская библиотека'})
 
 def about(request):
     books_list = Book.objects.all()
-    return render(request, 'about.html', {'title': 'Каталог книг', 'books': books_list})
+    return render(request, 'about.html', {'menu': Nav_Tables, 'title': 'Каталог книг', 'books': books_list})
 
 def adress(request):
-    return render(request, 'adress.html', {'menu': Nav_Tables})
+    return render(request, 'adress.html', {'menu': Nav_Tables, 'menu': Nav_Tables})
 
 def catalog(request):
     books_list = Book.objects.all()
-    return render(request, 'catalog.html', {'title': 'Каталог книг', 'books': books_list})
+    return render(request, 'catalog.html', {'menu': Nav_Tables, 'title': 'Каталог книг', 'books': books_list})
 
 def personal_area(request):
     return render(request, 'personal_area.html', {'menu': Nav_Tables})
 
 def managment(request):
-    return render(request, 'managment.html', {'menu': Nav_Tables})
+    return render(request, 'managment.html')
--- uppp_project/uppp_project/urls.py
+++ uppp_project/uppp_project/urls.py
@@ -3,18 +3,18 @@ from uppp_project import settings
 
 # Uncomment next two lines to enable admin:
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from library import views
 from django.conf.urls.static import static
 urlpatterns = [
     # Uncomment the next line to enable the admin:
     path('admin/', admin.site.urls),
-    path('', views.index),
-    path('about/', views.about),
-    path('catalog/', views.catalog),
-    path('adress/', views.adress),
-    path('profile/', views.personal_area),
-    path('manager/', views.managment)
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('catalog/', views.catalog, name='catalog'),
+    path('adress/', views.adress, name='adress'),
+    path('profile/', views.personal_area, name='profile'),
+    path('manager/', views.managment, name='manager')
 ]
 
 if settings.DEBUG:
