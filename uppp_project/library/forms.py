ca54a874c89ec4238df375a686537a05849b0b96
alkogenius21 <83871835+alkogenius21@users.noreply.github.com>
2023-06-16 22:11:12 +0300

    доделал личный кабинет пользователя,  сделал пару миграций, сделал возможность добавить в избранное

--- uppp_project/library/forms.py
+++ uppp_project/library/forms.py
@@ -1,3 +1,4 @@
+# -*- coding: cp1251 -*-
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import LibraryUser, Book, News_paper
@@ -23,3 +24,13 @@ class NewsForm(forms.ModelForm):
     class Meta:
         model = News_paper
         fields = '__all__'
+
+class EditProfileForm(forms.Form):
+    class Meta:
+        model = LibraryUser
+        fields = ('username', 'first_name', 'second_name', 'last_name', 'date_of_birth', 'email', 'phone')
+
+class ProblemReportForm(forms.Form):
+    problem_title = forms.CharField(label='Название проблемы', max_length=100)
+    problem_description = forms.CharField(label='Описание проблемы', widget=forms.Textarea)
+    problem_photo = forms.ImageField(label='Фото проблемы', required=False)
